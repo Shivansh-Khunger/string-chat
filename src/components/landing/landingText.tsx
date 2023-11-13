b227eed2ed0ab80b48bf9830084d89051fc91045
Shivansh Khunger <khunger.shivansh@outlook.com>
2023-11-13 18:42:19 +0530

    chnaged txt color

--- src/components/landing/landingText.tsx
+++ src/components/landing/landingText.tsx
@@ -4,11 +4,11 @@ export function LandingText() {
       <h1 className="scroll-m-20 text-5xl font-extrabold tracking-tight lg:text-5xl">
         want your talks to be
         <br />
-        <span className="font-outline-2 mx-2 tracking-wide text-slate-100 drop-shadow-[0_2.2px_0px_rgba(0,0,0,0.8)]">
+        <span className="font-outline-2 mx-2 tracking-wide text-slate-200 drop-shadow-[0_2.2px_0px_rgba(0,0,0,0.8)]">
           direct
         </span>
         <span className="font-outline-2 mx-2 font-bold">&</span>
-        <span className=" font-outline-2 mx-2 tracking-wide text-slate-100 drop-shadow-[0_2.2px_0px_rgba(0,0,0,0.8)]">
+        <span className=" font-outline-2 mx-2 tracking-wide text-slate-200 drop-shadow-[0_2.2px_0px_rgba(0,0,0,0.8)]">
           private
         </span>
       </h1>
